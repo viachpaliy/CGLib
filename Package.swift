136901f04a26f617c0c886ad1c7887fbfce6fb11
Rene Hexel <r.hexel@griffith.edu.au>
2017-09-24 12:32:46 +1000

    Add pkgConfig and providers

--- Package.swift
+++ Package.swift
@@ -1,3 +1,9 @@
 import PackageDescription 
 
-let package = Package(name: "CGLib")
+let package = Package(name: "CGLib",
+	pkgConfig: "glib-2.0 gio-unix-2.0",
+	providers: [
+		.Brew("glib glib-networking gobject-introspection"),
+		.Apt("libglib2.0-dev glib-networking gobject-introspection libgirepository1.0-dev")
+	]
+)
