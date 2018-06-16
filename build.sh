9071d126848df28f3f890c4214c01097b7eaaa6d
viachpaliy <viachpaliy@ukr.net>
2018-06-16 22:08:27 +0300

    add build command for Linux

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 #
-export PATH=/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin:"${PATH}"
-exec swift build -Xlinker -L/usr/local/lib -Xcc -I/usr/local/include/glib-2.0/ -Xcc -I/usr/local/include "$@"
+if [ "$OSTYPE" = "linux-gnu" ]; then
+      exec swift build -Xlinker -L/usr/lib -Xcc -I/usr/include/glib-2.0/ -Xcc -I/usr/include "$@"
+elif [ "$OSTYPE" = "darwin" ]; then
+       export PATH=/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin:"${PATH}"
+       exec swift build -Xlinker -L/usr/local/lib -Xcc -I/usr/local/include/glib-2.0/ -Xcc -I/usr/local/include "$@"
+fi
