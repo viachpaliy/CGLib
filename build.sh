57a346b6ff6995a477000b244acdb90561aa066e
viachpaliy <viachpaliy@ukr.net>
2018-06-17 10:43:48 +0300

    use pkg-config

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 #
 if [ "$OSTYPE" = "linux-gnu" ]; then
-      exec swift build -Xlinker -L/usr/lib -Xcc -I/usr/include/glib-2.0/ -Xcc -I/usr/include "$@"
+       LINKFLAGS=`pkg-config --libs gobject-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xlinker /' | tr '\n' ' '`
+       CCFLAGS=`pkg-config --cflags gobject-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xcc /' | tr '\n' ' ' `
+       exec swift build $CCFLAGS $LINKFLAGS "$@"
+   
 elif [ "$OSTYPE" = "darwin" ]; then
        export PATH=/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin:"${PATH}"
        exec swift build -Xlinker -L/usr/local/lib -Xcc -I/usr/local/include/glib-2.0/ -Xcc -I/usr/local/include "$@"
