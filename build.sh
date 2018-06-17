84d9c385d9f6efb99f6d11fd8bba4ba35f32f504
viachpaliy <viachpaliy@ukr.net>
2018-06-17 14:10:09 +0300

    change for build in Linux

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 #
-if [ "$OSTYPE" = "linux-gnu" ]; then
-       LINKFLAGS=`pkg-config --libs gobject-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xlinker /' | tr '\n' ' '`
-       CCFLAGS=`pkg-config --cflags gobject-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xcc /' | tr '\n' ' ' `
-       exec swift build $CCFLAGS $LINKFLAGS "$@"
-   
-elif [ "$OSTYPE" = "darwin" ]; then
-       export PATH=/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin:"${PATH}"
-       exec swift build -Xlinker -L/usr/local/lib -Xcc -I/usr/local/include/glib-2.0/ -Xcc -I/usr/local/include "$@"
-fi 
+export PATH=/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin:"${PATH}"
+LINKFLAGS=`pkg-config --libs gobject-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xlinker /' | tr '\n' ' '`
+CCFLAGS=`pkg-config --cflags gobject-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xcc /' | tr '\n' ' ' `
+exec swift build $CCFLAGS $LINKFLAGS "$@"
